d6459d5089dcb03476b1645d2d6c1a60887243af
shreyasshettyb <shreyasb52@gmail.com>
2023-10-24 15:44:42 +0530

    Fixed UI Pages

--- app/src/main/java/com/example/godappmaps/activities/GetStartedActivity.kt
+++ app/src/main/java/com/example/godappmaps/activities/GetStartedActivity.kt
@@ -3,6 +3,7 @@ package com.example.godappmaps.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import com.example.godappmaps.R
 
@@ -14,7 +15,7 @@ class GetStartedActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_started)
 
-        getStarted = findViewById(R.id.button_GetStarted)
+        getStarted = findViewById(R.id.btn_get_started)
         getStarted?.setOnClickListener {
             val intent = Intent(this, GetInActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or
@@ -23,4 +24,7 @@ class GetStartedActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    fun navigateToLoginFormActivity(view: View) {}
+    fun navigateToRegistrationForm(view: View) {}
 }
