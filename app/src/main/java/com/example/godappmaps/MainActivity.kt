f6d48d5ba652c2d538bce54da3509db49963378f
shreyasshettyb <shreyasb52@gmail.com>
2023-10-26 20:46:27 +0530

    Fixed Maps And Images

--- app/src/main/java/com/example/godappmaps/MainActivity.kt
+++ app/src/main/java/com/example/godappmaps/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.godappmaps.activities.GetStartedActivity
+import com.example.godappmaps.activities.LandDetails
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
