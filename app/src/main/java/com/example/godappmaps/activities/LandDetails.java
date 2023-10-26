155708a5ab6438cdf1e960cbce680c2e9372875d
shreyasshettyb <shreyasb52@gmail.com>
2023-10-26 18:36:02 +0530

    Fixed Land Pages

---
+++ app/src/main/java/com/example/godappmaps/activities/LandDetails.java
@@ -0,0 +1,16 @@
+package com.example.godappmaps.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.godappmaps.R;
+
+public class LandDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_land_details);
+    }
+}
